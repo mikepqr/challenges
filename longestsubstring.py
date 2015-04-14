95b039a3eab6ab44360500343b0943b1e6afd9dd
Mike Williams <mike@pentangle.net>
2015-04-13 20:40:54 -0400

    Move test function into separate file

--- longestsubstring.py
+++ longestsubstring.py
@@ -74,28 +74,3 @@ def length_of_longest_substring_slow(s):
         i += 1
 
     return longest
-
-
-def test_longest_substring(f=length_of_longest_substring):
-    tests = [('a', 1),
-             ('aa', 1),
-             ('abcabcda', 4),
-             ('dvdx', 3),
-             ('anviaj', 5),
-             (open('longstring.txt').read(), 95)]
-
-    for s, desired_result in tests:
-        if len(s) > 60:
-            s_ = s[0:60] + "... (truncated)"
-        else:
-            s_ = s
-
-        actual_result = f(s)
-
-        try:
-            assert actual_result == desired_result
-            print s_, "... OK!"
-        except AssertionError:
-            print "{0}(\"{1}\") = {2}. Should be {3}!".format(
-                f.func_name, s_, str(actual_result), str(desired_result))
-            raise
