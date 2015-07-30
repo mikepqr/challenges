86559aeeea95352c96fa881e9d607735b1b2a40a
Mike Williams <mike@pentangle.net>
2015-07-30 16:19:44 -0400

    Prototype blackjack implementation with same API

--- tictactoe.py
+++ tictactoe.py
@@ -2,8 +2,85 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 import itertools
+import random
 PLAYERS = ['❌', '😎']
 STALEMATE = 1
+
+
+class Blackjack(object):
+    '''
+    TODO:
+     - handle ties
+     - cards should be a deck with state
+     - handle situation when hand is five cards
+     - test with > 2 players
+    '''
+    CARD_NAME = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
+                 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']
+    CARD_VALUE = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    LIMIT = 21
+
+    def __init__(self):
+        self.hands = dict([(p, []) for p in PLAYERS])
+        self.player_order = iter(PLAYERS)
+        self.current_player = self.player_order.next()
+        self.winner = None
+
+    def _player_total(self, player=None):
+        if not player:
+            player = self.current_player
+        return sum([v for n, v in self.hands[player]])
+
+    def _print_player_hand_as_string(self, player=None):
+        if not player:
+            player = self.current_player
+        return ' '.join([str(v) for n, v in self.hands[player]])
+
+    def _determine_winner(self):
+        best_hand = 0
+        for p in self.hands:
+            if best_hand < self._player_total(player=p) <= Blackjack.LIMIT:
+                self.winner = p
+                best_hand = self._player_total(player=p)
+
+    def turn(self):
+        while self._get_decision():
+            self.hands[self.current_player].append(self._get_card())
+            print(self._print_player_hand_as_string())
+            print('= {}'.format(self._player_total()))
+            if self._player_total() >= Blackjack.LIMIT:
+                print('{} busts.'.format(self.current_player))
+                try:
+                    self.current_player = self.player_order.next()
+                except:
+                    self._determine_winner()
+                    return True
+
+        print('{} sticks on {}.'.format(self.current_player,
+                                        self._player_total()))
+
+        try:
+            self.current_player = self.player_order.next()
+        except:
+            self._determine_winner()
+            return True
+
+    def _get_card(self):
+        card_id = random.choice(range(1, 13))
+        card = (Blackjack.CARD_NAME[card_id], Blackjack.CARD_VALUE[card_id])
+        return card
+
+    def _get_decision(self):
+        print('Your move, {}'.format(self.current_player))
+        return True if raw_input('Another card? ') else False
+
+    def __repr__(self):
+        s = ''
+        for p in self.hands:
+            s += p
+            s += ' '
+            s += self._player_hand_as_string(p)
+            s += '\n'
 
 
 class TicTacToe(object):
