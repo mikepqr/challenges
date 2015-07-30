f53ca787229edfae8696e49562ee561e05a31943
Mike Williams <mike@pentangle.net>
2015-07-30 13:47:48 -0400

    Tidy API, add docstrings, add emoji characters for players

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from __future__ import print_function
-PLAYERS = ['X', 'O']
+PLAYERS = ['❌', '😎']
 STALEMATE = 1
 
 
 class TicTacToe(object):
+    '''
+    Tic tac toe class. To play, create an instance and repeatedly call its
+    turn() method until it returns True, indicating the game is finished. The
+    instance's winner attribute is then the winner. Print the instance at any
+    time to render the current board.
+    '''
 
     def __init__(self):
         self.board = self._create_board()
@@ -11,9 +19,18 @@ class TicTacToe(object):
         self.winner = None
 
     def _create_board(self):
+        '''
+        Board is represented as a dictionary with keys 1..9. Values are
+        initially 1..9, but are changed to 'X' or 'O' by turn() as moves are
+        made.
+        '''
         return dict(zip(range(1, 10), range(1, 10)))
 
-    def is_finished(self):
+    def _is_finished(self):
+        '''
+        Returns True if game finished (by victory or stalemate), False
+        otherwise.
+        '''
         lines = ((1, 2, 3), (4, 5, 6), (7, 8, 9),
                  (1, 4, 7), (2, 5, 8), (3, 6, 9),
                  (1, 5, 9), (3, 5, 7))
@@ -32,23 +49,31 @@ class TicTacToe(object):
 
         return False
 
-    def add_move(self):
-        move = self.get_move()
+    def turn(self):
+        '''
+        Gets and plays turn. Returns True if game is finished, False otherwise.
+        '''
+        move = self._get_move()
         self.board[move] = self.next_player
         self.next_player = (PLAYERS[0] if self.next_player == PLAYERS[1]
                             else PLAYERS[1])
+        return self._is_finished()
 
     def _check_move(self, move):
+        '''
+        Checks if input is an integer from 1..9, and that that location on
+        board is empty.
+        '''
         try:
             move = int(move)
             assert type(move) is int
             assert 1 <= move <= 9
-            assert self.board[move] not in PLAYERS
+            assert self.board[move] == move  # i.e. location is empty
             return True
         except:
             return False
 
-    def get_move(self):
+    def _get_move(self):
         print('Your move, {}'.format(self.next_player))
         print(self)
         move = None
@@ -65,11 +90,18 @@ class TicTacToe(object):
         return s
 
 
-def main():
-    game = TicTacToe()
+def main(game=TicTacToe()):
+    '''
+    Plays a game that implements the following API (tic tac toe by default):
 
-    while not game.is_finished():
-        game.add_move()
+     - a turn() method that takes a turn, and returns True if the game is
+       finished.
+     - a winner attribute that contains the string value of the winner.
+
+    '''
+
+    while not game.turn():
+        pass
 
     if game.winner != STALEMATE:
         print('Winner: {}'.format(game.winner))
